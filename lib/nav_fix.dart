f38a0ced3f95ecd8d0e90124125ba0ca22bae5eb
Gabriel Souza <gabriel.souza@hostaraguaia.com.br>
2023-04-09 20:41:43 -0300

    Updates description

--- lib/nav_fix.dart
+++ lib/nav_fix.dart
@@ -1,7 +1,7 @@
 import 'nav_fix_platform_interface.dart';
 
 class NavFix {
-  /// Goes back to the previous screen registered in the navigation history.
+  /// Goes back to the previous screen registered in the navigator history.
   static void pop() {
     return NavFixPlatform.instance.pop();
   }
